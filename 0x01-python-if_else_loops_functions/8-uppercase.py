c1d88df225ea60c04620e801f6d1fb9a52761774
Mekonen-Abera <makoninabera@gmail.com>
2023-11-01 08:49:36 -0700

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,10 @@
-#!/usr/bin/env python3
-uppercase = __import__('8-uppercase').uppercase
+#!/usr/bin/python3
 
-uppercase("best")
-uppercase("Best School 98 Battery street")
+
+def uppercase(str):
+    for i in range(len(str)):
+        uni_code = ord(str[i])
+        if uni_code >= 97 and uni_code <= 122:
+            uni_code = uni_code - 32
+        print("{}".format(chr(uni_code)), end='')
+    print()
