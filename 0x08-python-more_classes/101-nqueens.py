16912b1a9f18c9a36b84c17a8cfd728ea8d014ff
Mekonen-Abera <makoninabera@gmail.com>
2023-11-27 03:27:33 -0800

    Python - More Classes and Objects

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""The N queens puzzle is the challenge of placing 
-N non-attacking queens on an N×N chessboard. 
-Write a program that solves the N queens problem.
+"""
+This module contains an algorithm that resolves the N-Queen puzzle
+using backtracking
 """
 
 
