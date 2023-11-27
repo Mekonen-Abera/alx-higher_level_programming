cce79d8cc74df2eba7bf642dae119c8df5031d69
Mekonen-Abera <makoninabera@gmail.com>
2023-11-27 03:48:57 -0800

    The N queens puzzle

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -4,6 +4,8 @@
 Determines all possible solutions to placing N
 N non-attacking queens on an NxN chessboard.
 
+Done by: Mekonen Abera. Addis Ababa, Ethiopia, Nov. 2023
+
 Example:
     $ ./101-nqueens.py N
 
@@ -12,10 +14,6 @@ N must be an integer greater than or equal to 4.
 Attributes:
     board (list): A list of lists representing the chessboard.
     solutions (list): A list of lists containing solutions.
-
-Solutions are represented in the format [[r, c], [r, c], [r, c], [r, c]]
-where `r` and `c` represent the row and column, respectively, where a
-queen must be placed on the chessboard.
 """
 import sys
 
