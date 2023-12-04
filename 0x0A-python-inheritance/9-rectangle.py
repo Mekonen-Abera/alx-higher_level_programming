7a0f610a5541100578db9d7861ee21fc99faf65e
Mekonen-Abera <makoninabera@gmail.com>
2023-12-04 04:57:31 -0800

    Full rectangle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -4,6 +4,7 @@ that inherits from BaseGeometry."""
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """Represent a rectangle using BaseGeometry."""
 
