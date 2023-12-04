085cbca046b19dd21ffed49ce462f123f1c7b438
Mekonen-Abera <makoninabera@gmail.com>
2023-12-04 04:06:36 -0800

    Exact same object

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""
-Write a function that returns True if the object is exactly 
-an instance of the specified class ; otherwise False.
-"""
+"""Module containing is_same_class method"""
+
 
 def is_same_class(obj, a_class):
     """Returns:
