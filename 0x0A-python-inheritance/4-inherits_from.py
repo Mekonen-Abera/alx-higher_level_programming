08819243b75bac613fd269fc6c10c5439a67f401
Mekonen-Abera <makoninabera@gmail.com>
2023-12-04 04:32:13 -0800

    Only sub class of

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Define Module containing inherits_from method"""
+"""Module containing inherits_from method"""
 
 
 def inherits_from(obj, a_class):
-    """Reurns True if the object is an instance of a class that inherited 
-    (directly or indirectly) from the specified class ; otherwise False """
-    
+    """returns True if the object is an instance of a class that
+    inherited (directly or indirectly) from the specified class;
+    otherwise False"""
     return isinstance(obj, a_class) and type(obj) != a_class
