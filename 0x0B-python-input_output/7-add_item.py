a7f0d651646f6d946bc562cfe29526bb0318a010
Mekonen-Abera <makoninabera@gmail.com>
2023-12-05 03:31:33 -0800

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """A script that adds all arguments to a Python list"""
+
 import sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
@@ -9,11 +10,9 @@ try:
     lst = load_from_json_file("add_item.json")
 except:
     lst = []
-
-argc = len(sys.argv)
+    argc = len(sys.argv)
 
 if argc > 1:
     for i in range(1, argc):
         lst.append(sys.argv[i])
-
-save_to_json_file(lst, "add_item.json")
+        save_to_json_file(lst, "add_item.json")
