5beb7234b009caf8bc0ceea695ec71f8caccbb83
Mekonen-Abera <makoninabera@gmail.com>
2023-12-05 05:27:47 -0800

    Load Parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -16,7 +16,6 @@ def print_stats(size, status_codes):
 if __name__ == "__main__":
     import sys
 
-
     size = 0
     status_codes = {}
     valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
@@ -49,7 +48,5 @@ if __name__ == "__main__":
         print_stats(size, status_codes)
 
     except KeyboardInterrupt:
-
         print_stats(size, status_codes)
         raise
-
