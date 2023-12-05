ab73ed3c23b9b9192b9d8584561f05433f3b4448
Mekonen-Abera <makoninabera@gmail.com>
2023-12-05 05:22:24 -0800

    Log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-"""Reads from standard input and computes metrics.
-
-After every ten lines or the input of a keyboard interruption (CTRL + C),
-prints the following statistics:
-    - Total file size up to that point.
-    - Count of read status codes up to that point.
-"""
+"""A script that reads stdin line by line and computes metrics:"""
 
 
 def print_stats(size, status_codes):
     """Print accumulated metrics.
-
     Args:
         size (int): The accumulated read file size.
         status_codes (dict): The accumulated count of status codes.
@@ -20,10 +13,9 @@ def print_stats(size, status_codes):
     for key in sorted(status_codes):
         print("{}: {}".format(key, status_codes[key]))
 
-
 if __name__ == "__main__":
-
     import sys
+
 
     size = 0
     status_codes = {}
