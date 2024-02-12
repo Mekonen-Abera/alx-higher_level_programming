8b26b82dc60159be3782ffc5a6c7ffa93f64c994
Mekonen-Abera <makoninabera@gmail.com>
2024-02-12 03:57:58 -0800

    Value of my argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined) {
-  console.log('No argument');
+
+const myArg = process.argv[2];
+
+if (myArg) {
+  console.log(myArg);
 } else {
-  console.log(process.argv[2]);
-} 
+  console.log('No argument');
+}
