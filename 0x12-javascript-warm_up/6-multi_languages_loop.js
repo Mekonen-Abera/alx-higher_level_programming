d56d6281991ac84a307cc6f884971083f796ecb9
Mekonen-Abera <makoninabera@gmail.com>
2024-02-12 04:02:29 -0800

    Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
-  console.log(line);
+
+const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (let index = 0; index < myArray.length; index++) {
+  const element = myArray[index];
+  console.log(element);
 }
