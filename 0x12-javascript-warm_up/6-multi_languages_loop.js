6aa51df181750b3e42fde8b3de79806250b658aa
Mekonen-Abera <makoninabera@gmail.com>
2024-02-12 22:49:07 -0800

    Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,7 +2,7 @@
 
 const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0; i < languages.length; i++) {
+for (let i = 0; i < languages.length; i++)
 	const array=languages[i];
-	console.log(array);
+console.log(array);
 } 
