c1cb9aa5e005b97f950ec979fc8d56c16e782348
Mekonen-Abera <makoninabera@gmail.com>
2024-02-16 04:22:58 -0800

    Add a new state

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python3
-"""
-Adds the State object "Louisiana" to the database hbtn_0e_6_usa.
-"""
-
+'''Adds a State object to a database.
+'''
 import sys
-from unicodedata import name
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
 
-if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    session_maker = sessionmaker(bind=engine)
-    session = session_maker()
+from model_state import Base, State
 
-    obj = State(name="Louisiana")
-    session.add(obj)
-    session.commit()
-    print(obj.id)
+
+if __name__ == '__main__':
+    if len(sys.argv) >= 4:
+        user = sys.argv[1]
+        pword = sys.argv[2]
+        db_name = sys.argv[3]
+        DATABASE_URL = "mysql://{}:{}@localhost:3306/{}".format(
+            user, pword, db_name
+        )
+        engine = create_engine(DATABASE_URL)
+        Base.metadata.create_all(engine)
+        session = sessionmaker(bind=engine)()
+        new_state = State(name='Louisiana')
+        session.add(new_state)
+        try:
+            session.flush()
+            session.refresh(new_state)
+            if new_state.id is not None:
+                print('{}'.format(new_state.id))
+        except Exception:
+            session.rollback()
+        finally:
+            session.commit()
