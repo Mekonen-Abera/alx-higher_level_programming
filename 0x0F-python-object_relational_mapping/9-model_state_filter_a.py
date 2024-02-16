268967ef388110f67ca7f3af2b69410d0812b4fe
Mekonen-Abera <makoninabera@gmail.com>
2024-02-16 04:04:57 -0800

    Contains

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Lists all State objects that contain the letter 'a' 
+''' Lists all State objects that contain the letter 'a' 
 from the database hbtn_0e_6_usa.
-"""
+''' 
 
 import imp
 import sys
