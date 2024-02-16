54c1b7f84a7c27884c08b90779b190e2f6f4c2a4
Mekonen-Abera <makoninabera@gmail.com>
2024-02-16 04:36:35 -0800

    Relationship State

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
-"""
-Defines a state model that contains the class definition of 
-a State and an instance Base = declarative_base().
-"""
-from lib2to3.pytree import Base
-from sre_parse import State
-from unicodedata import name
+'''A module containing the State model.
+'''
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-from relationship_city import Base, City
+from sqlalchemy.orm import relationship
+
+
 Base = declarative_base()
+'''Represents the base class for all tables.
+'''
 
 
 class State(Base):
-"""
-Inherits from Base Tips and links to the MySQL table states.
-Class attribute id represents a column of an auto-generated, unique integer, can't be null, and is a primary key.
-Class attribute name represents a column of a string with a maximum of 128 characters and can't be null.
-"""
-
- __tablename__ = "states"
-    id = Column(Integer, primary_key=True)
-    name = Column(String(128), nullable=False)
-
-    cities = relationship("City", backref="state", cascade="all, delete")
+    '''Represents a row in a states table.
+    '''
+    __tablename__ = "states"
+    id = Column(
+        Integer,
+        autoincrement=True,
+        unique=True,
+        nullable=False,
+        primary_key=True
+    )
+    name = Column(
+        String(length=128),
+        nullable=False
+    )
+    cities = relationship(
+        "City",
+        cascade="all, delete, delete-orphan",
+        backref="state"
+    )
