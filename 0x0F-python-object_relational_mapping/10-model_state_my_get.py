d5ba29acc22dbaefea45652428b041b90a8f2eb6
Mekonen-Abera <makoninabera@gmail.com>
2024-02-16 04:11:12 -0800

    Get a state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,23 +1,30 @@
 #!/usr/bin/python3
-"""
-Prints the State object with the name passed as an argument 
-from the database hbtn_0e_6_usa.
-"""
-
+'''Prints the id of a State object with a given name in a database.
+'''
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
 
-if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    session_maker = sessionmaker(bind=engine)
-    session = session_maker()
+from model_state import Base, State
 
-    for state in session.query(State):
-        if sys.argv[4] == state.name:
-            print("{}".format(state.id))
-            break
-    else:
-        print("Not found")
+
+if __name__ == '__main__':
+    if len(sys.argv) >= 5:
+        user = sys.argv[1]
+        pword = sys.argv[2]
+        db_name = sys.argv[3]
+        state_name = sys.argv[4]
+        chk = map(lambda x: x.isalpha() or (x in (' ', '%', '_')), state_name)
+        if not all(chk):
+            state_name = ''
+        DATABASE_URL = "mysql://{}:{}@localhost:3306/{}".format(
+            user, pword, db_name
+        )
+        engine = create_engine(DATABASE_URL)
+        Base.metadata.create_all(engine)
+        session = sessionmaker(bind=engine)()
+        result = session.query(State).filter(State.name == state_name).first()
+        if result is not None:
+            print('{}'.format(result.id))
+        else:
+            print('Not found')
