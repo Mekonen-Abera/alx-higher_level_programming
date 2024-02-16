ddb386a8d2155b0a7a006facacebe5e6ca75e4fb
Mekonen-Abera <makoninabera@gmail.com>
2024-02-16 04:06:48 -0800

    Contains

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 ''' Lists all State objects that contain the letter 'a' 
-from the database hbtn_0e_6_usa.
-''' 
-
+from the database hbtn_0e_6_usa.''' 
 import imp
 import sys
 from venv import create
@@ -20,3 +18,4 @@ if __name__ == "__main__":
     for state in session.query(State).order_by(State.id):
         if "a" in state.name:
             print("{}: {}".format(state.id, state.name))
+
