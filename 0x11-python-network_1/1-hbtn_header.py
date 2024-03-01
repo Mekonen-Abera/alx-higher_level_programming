2db664beed8edcf3f713adbd299b7e494e0a104a
Mekonen-Abera <makoninabera@gmail.com>
2024-03-01 03:19:10 -0800

    Response header value

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+Script that accepts a URL, sends a request to the URL, 
+and retrieves the value of the X-Request-Id variable from the response header.
+
+Usage: ./1-hbtn_header.py <URL>
+"""
+from sys import argv
+from urllib.request import Request, urlopen
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    req = Request(url)
+
+    with urlopen(req) as response:
+        print(dict(response.headers).get("X-Request-Id"))
