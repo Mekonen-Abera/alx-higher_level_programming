4f2a00e8b106ff50614d0afa79168a7954a79274
Mekonen-Abera <makoninabera@gmail.com>
2024-03-01 03:40:46 -0800

    Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+Script that accepts a URL, sends a request to the URL, and displays the response
+body decoded in utf-8. It also handles HTTP errors.
+
+Usage: ./3-error_code.py <URL>
+"""
+from sys import argv
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    req = Request(url)
+
+    try:
+        with urlopen(req) as response:
+            print(response.read().decode("ascii"))
+    except HTTPError as e:
+        print("Error code: {}".format(e.code))
