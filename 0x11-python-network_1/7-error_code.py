6d93381ae2f06dafb0606a2f006190ccf9dae399
Mekonen-Abera <makoninabera@gmail.com>
2024-03-01 04:11:43 -0800

    Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+Script that receives a URL, sends a request to the URL
+and shows the response body.
+
+Usage: ./7-error_code.py <URL>
+ - Manages HTTP errors. 
+"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    req = requests.get(url)
+
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
